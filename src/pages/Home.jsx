c18957c650016cf4eb0d4cfa7b315d91470629aa
CallMeTwist <sukaxavier@gmail.com>
2025-09-29 05:07:32 -0700

    reconnect project to repo plus new changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import {ThemeToggle} from "@/components/ThemeToggle.jsx";
+import { StarBackground} from "@/components/StarBackground.jsx";
+import {Navbar} from "@/components/Navbar.jsx";
+import {HeroSection} from "@/components/HeroSection.jsx";
+import { AboutSection } from "../components/AboutSection";
+import {SkillsSection} from "@/components/SkillsSection.jsx";
+import {ProjectsSection} from "@/components/ProjectsSection.jsx";
+
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/*  Theme Toggle  */}
+        <ThemeToggle />
+        {/*  Background Effects  */}
+        <StarBackground />
+        {/*  Navbar  */}
+        <Navbar />
+        {/*  Main Content  */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <SkillsSection />
+            <ProjectsSection />
+        </main>
+        {/*  footer  */}
+
+        </div>
+    )
+}
