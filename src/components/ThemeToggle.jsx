8828266bc308932206e3b05d544d6bb5777c2534
CallMeTwist <sukaxavier@gmail.com>
2025-09-30 09:06:05 -0700

    some fixes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import {cn} from "@/lib/utils.js";
 
 export const ThemeToggle = () => {
-    const [isDardMode, setIsDardMode] = useState(false);
+    const [isDardMode, setIsDardMode] = useState(true);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
